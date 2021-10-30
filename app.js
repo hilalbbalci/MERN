3f5f9883eb738b28e2fff32d4c7b49f98223b228
hilalbbalci <drhilalbiten@gmail.com>
2021-10-30 10:42:43 -0700

    my first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
